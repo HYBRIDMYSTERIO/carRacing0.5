52c5e4d3ac6da530fcdd5ddd384e4a5380da25b0
HYBRIDMYSTERIO <hybridmysteryo@gmail.com>
2020-11-25 17:50:38 +0530

    game

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,7 @@ class Game {
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
        gameState = data.val();
-    })
+    });
    
   }
 
@@ -15,12 +15,83 @@ class Game {
     });
   }
 
-  start(){
+async  start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
       form = new Form()
       form.display();
+      }
+
+
+      
     }
-  }
 }
+
+  /*play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120,60);
+    Player.getPlayerInfo();
+    if(players !== undefined){
+      var displayPosition = 130;
+      
+      for(var plr in players){
+        if(plr === "player" + player.index)
+          fill("red");
+
+        else
+        
+          fill("black");
+
+        displayPosition = displayPosition + 40;
+        text(players[plr].name + ":" + players[plr].distance, 120, displayPosition );
+          
+        
+
+        
+      }
+
+    }
+  }*/
+    /*if(keyIsDown(UP_ARROW) && player.index !== null){
+       player.distance += 50;
+       player.update();
+    }*/
+    play(){ 
+      form.hide();
+       textSize(30); 
+       text("Game Start", 120, 100) 
+       Player.getPlayerInfo(); 
+       if(players !== undefined)
+       { 
+         var display_position = 130;
+          for(var plr in players)
+          { 
+            if (plr === "player" + player.index)
+             fill("red")
+              else 
+              fill("black"); 
+              display_position+=20; 
+              textSize(15); 
+              text(players[plr].name + ": " + players[plr].distance, 120,display_position) 
+            }
+       } 
+             if(keyIsDown(UP_ARROW) && player.index !== null)
+             { 
+               player.distance +=50 
+               player.update(); 
+             } 
+          }
+ }
+
+        
+        
+
+
+  
+
+
